449661a0974cdd7e492f4e8beff2622242c4b869
Deborah <liz.joy43@gmail.com>
2025-01-18 21:57:41 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header className="App-Header"></header>
+      <h1>My Dictionary Project</h1>
+    </div>
+  );
+}
+
+export default App;
