a3dc5f78e6b93aa642fe0b4ffa745aa6ea568314
Deborah <liz.joy43@gmail.com>
2025-01-18 22:10:00 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,24 @@ function App() {
     <div>
       <header className="App-Header"></header>
       <h1>My Dictionary Project</h1>
+      <footer>
+        This page was coded by Deborah Chacko is open-sourced on
+        <a
+          href="https://github.com/Sapphire59"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        and is hosted on
+        <a
+          href="https://dc-react-dictionary-app.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
