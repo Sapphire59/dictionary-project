506e4fc09eec0fd0e11b49acb42b0678869eafd1
Deborah <liz.joy43@gmail.com>
2025-01-18 22:12:57 -0700

    added space and centered text

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-Header"></header>
       <h1>My Dictionary Project</h1>
       <footer>
-        This page was coded by Deborah Chacko is open-sourced on
+        This page was coded by Deborah Chacko is open-sourced on{" "}
         <a
           href="https://github.com/Sapphire59"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Github
+          Github{" "}
         </a>
-        and is hosted on
+        and is hosted on{" "}
         <a
           href="https://dc-react-dictionary-app.netlify.app/"
           target="_blank"
